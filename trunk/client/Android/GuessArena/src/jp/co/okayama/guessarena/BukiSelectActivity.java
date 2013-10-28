1e7af3dc0f503ee7fdeb4af2fa0a0a3cc4f8b789
tuandat87@gmail.com <tuandat87@gmail.com@b56dc916-7024-e035-1f72-d783eea0ca84>
2013-10-28 17:25:58 +0000

    BukiSelectActivity select buki before match starts

--- trunk/client/Android/GuessArena/src/jp/co/okayama/guessarena/BukiSelectActivity.java
+++ trunk/client/Android/GuessArena/src/jp/co/okayama/guessarena/BukiSelectActivity.java
@@ -1,5 +1,16 @@
 package jp.co.okayama.guessarena;
 
-public class BukiSelectActivity extends BaseActivity {
+import android.os.Bundle;
+import android.widget.GridView;
 
+public class BukiSelectActivity extends BaseActivity {
+    GridView mGridBuki;
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        
+        setContentView(R.layout.activity_buki_select);
+        mGridBuki = (GridView) findViewById(R.id.gridView_buki);
+        
+    }
 }
