2490be594635faab380158d8deff0c8c23f949c1
datdvt@gmail.com <datdvt@gmail.com@b56dc916-7024-e035-1f72-d783eea0ca84>
2013-10-29 16:22:00 +0000

    [Android] prototype for Wait

--- client/Android/GuessArena/src/jp/co/okayama/guessarena/WaitActivity.java
+++ client/Android/GuessArena/src/jp/co/okayama/guessarena/WaitActivity.java
@@ -1,5 +1,11 @@
 package jp.co.okayama.guessarena;
 
-public class WaitActivity extends BaseActivity {
+import android.os.Bundle;
 
+public class WaitActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_wait);
+    }
 }
