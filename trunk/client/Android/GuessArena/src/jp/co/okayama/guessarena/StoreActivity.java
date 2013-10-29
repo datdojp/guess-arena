d01b419f2138dd132bd1ea1ca9a626dfbb3661de
datdvt@gmail.com <datdvt@gmail.com@b56dc916-7024-e035-1f72-d783eea0ca84>
2013-10-29 15:34:14 +0000

    [Android] prototype for Store

--- trunk/client/Android/GuessArena/src/jp/co/okayama/guessarena/StoreActivity.java
+++ trunk/client/Android/GuessArena/src/jp/co/okayama/guessarena/StoreActivity.java
@@ -1,5 +1,30 @@
 package jp.co.okayama.guessarena;
 
-public class StoreActivity extends BaseActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
+public class StoreActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_store);
+
+        final View[] tabs = new View[] {
+                findViewById(R.id.tab_top),
+                findViewById(R.id.tab_category),
+                findViewById(R.id.tab_all)
+        };
+        for (int i = 0; i < tabs.length; i++) {
+            tabs[i].setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    for (int j = 0; j < tabs.length; j++) {
+                        tabs[j].setSelected(v == tabs[j]);
+                    }
+                }
+            });
+        }
+        tabs[0].performClick();
+    }
 }
