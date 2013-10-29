9a0278c49edccd568572d784d8364adf9b0822b2
datdvt@gmail.com <datdvt@gmail.com@b56dc916-7024-e035-1f72-d783eea0ca84>
2013-10-29 14:49:30 +0000

    [Android] prototype for Answer Guess

--- trunk/client/Android/GuessArena/src/jp/co/okayama/guessarena/AnswerGuessActivity.java
+++ trunk/client/Android/GuessArena/src/jp/co/okayama/guessarena/AnswerGuessActivity.java
@@ -1,5 +1,14 @@
 package jp.co.okayama.guessarena;
 
-public class AnswerGuessActivity extends BaseActivity {
+import jp.co.okayama.guessarena.util.Utils;
+import android.os.Bundle;
 
+public class AnswerGuessActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_answer_guess);
+        
+        Utils.focusNothing(this);
+    }
 }
